c9dce59be71a835179561ca9f15ec0d76d3ea569
amera-ux <amera.mussa@gmail.com>
2022-07-22 20:39:26 +0300

    Navbar and Hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Hero.css'
+
+
+
+const Hero = () => {
+    return(
+        <div className="hero">
+            <h1>Looking for music?</h1>
+            <p>Start listening now.</p>
+        </div>
+    )
+}
+
+export default Hero;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import './NavigationBar.css';
+
+
+
+const NavigationBar = ()=> {
+    return(
+      <header className="NavigationBar">
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Profile</li>
+              
+            </ul>
+          </nav>
+        </header>
+  
+    )
+  }
+
+  export default NavigationBar;
